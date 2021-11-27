0a4d57c5e61ad2f62ec39cc092d4d45a419ba6b4
Dorian Edwards <pharmdtechie@Dorians-MBP.fios-router.home>
2021-11-26 23:53:05 -0500

    add calculator functions, utility functions, and key bindings

--- script.js
+++ script.js
@@ -1,59 +1,116 @@
 let currentValue = ''
+let queuedValue = ''
 let decimal = false
 let locked = false
-let queuedValue = ''
-let storedOperation = ''
+let isRes = false
+let operator = null
 
-// calc functions
+const operators = {
+  '/': divide,
+  '*': multiply,
+  '-': subtract,
+  '+': add,
+}
+
+// value to be displayed MUST be a string
+// this function doesn't sanitize values to be displayed, probably should tho...
 const updateDisplay = () => {
-  if (currentValue) {
-    let place = 10
-    const end = currentValue.length - 1
-    for (let i = end; i >= 0; i--) {
-      const cell = document.querySelector(`.cell:nth-of-type(${place--})`)
-      cell.textContent = currentValue[i]
+  document.querySelectorAll('.cell').forEach((e) => (e.textContent = ''))
+  let place = 10
+  const end = currentValue.length - 1
+  for (let i = end; i >= 0; i--) {
+    const cell = document.querySelector(`.cell:nth-of-type(${place--})`)
+    cell.textContent = currentValue[i]
+  }
+}
+
+const handleKeyPress = (e) => {
+  const key = e.keyCode
+  if (key === 13 || key === 191) e.preventDefault()
+  if (key === 8) return backspace()
+  if (key === 67) return clear()
+
+  if (key === 13) return handleOperatorInput('=')
+
+  const search = /\d|[\/\*\-\+\=\.]/g
+  const match = search.exec(e.key)
+  if (match) {
+    const inpt = match[0]
+    if (inpt >= 0 && inpt <= 9) {
+      handleNumberInput(inpt)
+    } else if (inpt === '.') {
+      handleDec()
+    } else {
+      handleOperatorInput(inpt)
     }
   }
 }
 
 // input handlers
 const handleNumberInput = (e) => {
-  if (!locked && currentValue.length < 10) {
-    currentValue += e.target.value
-    updateDisplay()
-  }
-}
-
-const handleDec = (e) => {
-  if (!locked && !decimal) {
-    if (currentValue) {
-      currentValue += '.'
-      decimal = true
+  const num = e.target?.value || e
+  if (locked) return
+  if (currentValue.length < 10) {
+    if (isRes && operator) {
+      queuedValue = currentValue
+      currentValue = num
+      isRes = false
+    } else if (currentValue === '0' || (isRes && !operator)) {
+      currentValue = num
+      isRes = false
     } else {
-      currentValue = '0.'
-      decimal = true
+      currentValue += num
     }
     updateDisplay()
   }
 }
 
-const handleOperatorInput = (e) => {
-  console.log(e.target.value)
+const handleDec = () => {
+  if (locked) return
+  if (!decimal && !currentValue) {
+    currentValue += '0.'
+    decimal = true
+  }
+
+  if (!decimal && currentValue.length < 10) {
+    currentValue += '.'
+    decimal = true
+  }
+  updateDisplay()
 }
 
-const handleEquals = (e) => {
-  console.log(e.target.value)
+const handleOperatorInput = (e) => {
+  const input = e.target?.value || e
+  if (!currentValue || locked) return
+  if (currentValue && !operator && input === '=') return
+  if (currentValue && !operator) {
+    operator = operators[input]
+    queuedValue = currentValue
+    currentValue = ''
+    decimal = false
+    isRes = false
+  }
+  if (currentValue && queuedValue && operator) {
+    completeOperation()
+    if (input !== '=') {
+      operator = operators[input]
+    } else {
+      operator = null
+    }
+  }
 }
 
 const handleClear = () => {
-  currentValue = ''
-  decimal = false
-  locked = false
-  document.querySelectorAll('.cell').forEach((e) => (e.textContent = ''))
+  if (locked) return
+  clear()
+  console.clear()
 }
 
 const handleReset = () => {
-  console.log('reset')
+  locked = false
+  operator = null
+  clear()
+  reset()
 }
 
 // register event listeners
@@ -68,4 +125,83 @@ document.querySelectorAll('.btn.calc').forEach((e) => {
 document.querySelector('#clear').addEventListener('click', handleClear)
 document.querySelector('#reset').addEventListener('click', handleReset)
 document.querySelector('.btn.dec').addEventListener('click', handleDec)
-document.querySelector('.btn.equals').addEventListener('click', handleEquals)
+document.querySelector('#del').addEventListener('click', backspace)
+document.querySelector('body').addEventListener('keydown', handleKeyPress)
+
+// Utility Functions
+
+function backspace() {
+  if (locked) return
+  if (currentValue && !isRes) {
+    if (currentValue === '0.') {
+      currentValue = ''
+      decimal = false
+    } else if (currentValue.endsWith('.')) {
+      currentValue = currentValue.substring(0, currentValue.length - 1)
+      decimal = false
+    } else {
+      currentValue = currentValue.substring(0, currentValue.length - 1)
+    }
+  }
+}
+
+function clear() {
+  currentValue = ''
+  decimal = false
+  isRes = false
+  document.querySelectorAll('.cell').forEach((e) => (e.textContent = ''))
+}
+
+function reset() {
+  queuedValue = ''
+  decimal = false
+  locked = false
+  isRes = false
+}
+
+function sanitizeOutput() {
+  if (currentValue.length > 10) {
+    currentValue = Number(currentValue).toExponential(2).toString()
+    locked = true
+  }
+  if (!Number.isInteger(Number(currentValue))) {
+    currentValue = Number(currentValue).toFixed(2).toString()
+  }
+}
+
+// Calculations
+function add(a, b) {
+  return Number(a) + Number(b)
+}
+
+function subtract(a, b) {
+  return Number(a) - Number(b)
+}
+
+function multiply(a, b) {
+  return Number(a) * Number(b)
+}
+
+function divide(a, b) {
+  if (Number(b) === 0) {
+    return null
+  }
+
+  return Number(a) / Number(b)
+}
+
+function completeOperation() {
+  currentValue = operator(queuedValue, currentValue)
+  if (currentValue) {
+    currentValue = currentValue.toString()
+    sanitizeOutput()
+    reset()
+    isRes = true
+    sanitizeOutput()
+    updateDisplay()
+  } else {
+    currentValue = 'ERROR'
+    locked = true
+    updateDisplay()
+  }
+}
